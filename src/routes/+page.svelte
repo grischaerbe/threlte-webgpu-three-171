003142397f8f062c61c597b5ee58dcc21996eb35
Grischa Erbe <grischa.erbe@googlemail.com>
2024-12-01 22:20:17 +0100

    WebGPU test init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import { WebGPURenderer } from 'three/webgpu';
+	import Scene from './Scene.svelte';
+</script>
+
+<main>
+	<Canvas
+		createRenderer={(canvas) => {
+			return new WebGPURenderer({ canvas });
+		}}
+	>
+		<Scene />
+	</Canvas>
+</main>
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
---
+++ src/routes/Scene.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras';
+	import { color } from 'three/tsl';
+	import { MeshBasicNodeMaterial } from 'three/webgpu';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[0, 0, 10]}>
+	<OrbitControls />
+</T.PerspectiveCamera>
+
+<T.Mesh>
+	<T.BoxGeometry />
+	<T is={MeshBasicNodeMaterial} colorNode={color('#fff000')} />
+</T.Mesh>
